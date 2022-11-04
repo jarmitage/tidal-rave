91eaad68ea85349a65b3b379647cf6d2abd2cf3a
Jack Armitage <jack.armitage@me.com>
2022-11-04 11:13:33 +0000

    update

--- rave.hs
+++ rave.hs
@@ -15,7 +15,13 @@ raveTarget = Target {oName     = "RAVE",
 :{
 raveOSCSpecs = [OSC "/rave/{ravemodel}/mix"   $ ArgList [("ravemix", Nothing)],
                 OSC "/rave/{ravemodel}/prior" $ ArgList [("raveprior", Nothing)],
-                OSC "/rave/{ravemodel}/temp"  $ ArgList [("ravetemp", Nothing)]
+                OSC "/rave/{ravemodel}/temp"  $ ArgList [("ravetemp", Nothing)],
+                OSC "/rave/{ravemodel}/gain"  $ ArgList [("ravegain", Nothing)],
+                OSC "/rave/{ravemodel}/pan"   $ ArgList [("ravepan", Nothing)],
+                OSC "/rave/{ravemodel}/pitch/ratio" $ ArgList [("ravepitchratio", Nothing)],
+                OSC "/rave/{ravemodel}/pitch/grain" $ ArgList [("ravepitchgrain", Nothing)],
+                OSC "/rave/{ravemodel}/pitch/disperse" $ ArgList [("ravepitchdisperse", Nothing)],
+                OSC "/rave/{ravemodel}/pitch/time" $ ArgList [("ravepitchtimedisperse", Nothing)]
                ]
 :}
 
@@ -24,8 +30,29 @@ raveOSCSpecs = [OSC "/rave/{ravemodel}/mix"   $ ArgList [("ravemix", Nothing)],
 let ravemix   = pF "ravemix"
     raveprior = pI "raveprior"
     ravetemp  = pF "ravetemp"
+    ravegain  = pF "ravegain"
+    ravepan   = pF "ravepan"
     rave      = pS "ravemodel"
+:}
+
+-- RAVE PitchShift.ar
+:{
+let ravepitchratio = pF "ravepitchratio"
+    ravepitchgrain = pF "ravepitchgrain"
+    ravepitchdisperse = pF "ravepitchdisperse"
+    ravepitchtimedisperse = pF "ravepitchtimedisperse"
+:}
+
+-- RAVE Shortcuts
+:{
+let ravepitch r d t = ravepitchratio r # ravepitchdisperse d # ravepitchtimedisperse t
 :}
 
 -- RAVE OSC Map
 raveOscMap = (raveTarget, raveOSCSpecs)
+
+-- RAVE Utilities
+-- hushrave = once $ rave "[speech, darbouka, fallout]" # ravegain 0
+
+
+
